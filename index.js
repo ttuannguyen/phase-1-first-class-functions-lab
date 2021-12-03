6c6d760f7368a1a0cf9d50b32a9c4c52e4ad7919
Tuan Nguyen <trentontuannguyen@gmail.com>
2021-12-03 18:05:28 -0500

    commit tree 81550c02cdb44d0b384a77d914d9fb8652b26f03

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+//console.log(drivers.splice(0, 2));
+//console.log(drivers);
+
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0, 2);
+} 
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = number => function() {
+        return number ** 2;
+    }
+ 
+const fareDoubler = createFareMultiplier => createFareMultiplier * 2;
+
+const fareTripler = createFareMultiplier => createFareMultiplier * 3;
+
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) =>{
+    return returnFirstTwoDrivers(arrayOfDrivers);
+}
